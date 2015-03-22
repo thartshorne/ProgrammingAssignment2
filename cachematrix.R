d5a94160584c06fbc563b2ee5c7184bad8afb93c
Tom Hartshorne <thomas.hartshorne@trincoll.edu>
2015-03-22 10:11:51 -0400

    Added required functions
    Edited the document to include both the makeCacheMatrix and cacheSolve
    functions, as well as descriptions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is meant to use the scoping rules of R to store the 
+## inverse of a matrix, so that it can be called upon later.
 
-## Write a short comment describing this function
+## This function creates a special "matrix", which is really a list
+## containing a function to 
+##	1. set the values of the matrix
+##	2. get the values of the matrix
+##	3. set the value of the matrix
+##	4. get the value of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL 
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function () x
+	setInverse function(solve) i <<- solve
+	getInverse function() i
+	list(set=set, get=get,setInverse=setInverse,getInverse=getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function checks if there is a cached inverse, and if not 
+## computes the inverse and returns it.  
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse
+        if (!is.null(i)) {
+        	message("getting cached data")
+        	return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setInverse(i)
+        i
 }
+
+
+
